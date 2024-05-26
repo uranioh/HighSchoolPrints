6eb4e3b6b82a9ff61ff3eced5813fb7e018982c4
loworenzo <lorenzocaboni05@gmail.com>
2024-05-26 14:45:18 +0200

    Change credentials, delete account

--- account/index.php
+++ account/index.php
@@ -45,6 +45,28 @@ if (!$_conn) {
     <link rel="stylesheet" href="/account/style.css">
     <link rel="stylesheet" href="/main.css">
     <script src="https://kit.fontawesome.com/b4ad05c0d4.js" crossorigin="anonymous"></script>
+    <script>
+        function enableEditing() {
+            // Rendo i campi editabili
+            document.getElementById("name").readOnly = false;
+            document.getElementById("surname").readOnly = false;
+            document.getElementById("mail").readOnly = false;
+            document.getElementById("password").readOnly = false;
+            document.getElementById("class").disabled = false;
+
+            //Nascondo il pulsante Modifica e mostro i pulsanti Aggiorna, Annulla ed Elimina
+            document.getElementById("editButton").style.display = 'none';
+            document.getElementById("updateButton").style.display = 'inline';
+            document.getElementById("cancelButton").style.display = 'inline';
+            document.getElementById("deleteButton").style.display = 'inline';
+        }
+        function confermaEliminazione() {
+            var conferma = confirm("Sei sicuro di voler cancellare?");
+            if (conferma) {
+                document.getElementById("deleteForm").submit();
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -66,15 +88,84 @@ if (!$_conn) {
 
 <main>
     <section>
-        <h1>Salve, <?php echo htmlspecialchars($user_name); ?></h1>
-        <p>I tuoi dati:</p>
-        <p>Name: <?php echo htmlspecialchars($user_name); ?></p>
-        <p>Surname: <?php echo htmlspecialchars($user_surname); ?></p>
-        <p>Mail: <?php echo htmlspecialchars($user_mail); ?></p>
-        <p>Password: <?php echo htmlspecialchars($user_password); ?></p>
-        <p>Class: <?php echo htmlspecialchars($user_class); ?></p>
-        <!-- Add more user details here -->
-    </section>
+        <div>
+            <h1>I tuoi dati</h1>
+            <form id="deleteForm" action="/account/update.php" method="post">
+                <div>
+                    <label for="name">Nome</label>
+                    <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($user_name); ?>" readonly>
+                </div>
+                <div>
+                    <label for="surname">Cognome</label>
+                    <input type="text" id="surname" name="surname" value="<?php echo htmlspecialchars($user_surname); ?>" readonly>
+                </div>
+                <div>
+                    <label for="class">Classe</label>
+                    <select id="class" name="class" disabled>
+                        <option value="" disabled>Prime</option>
+                        <option value="1A" <?php if ($user_class == '1A') echo 'selected'; ?>>1A</option>
+                        <option value="1B" <?php if ($user_class == '1B') echo 'selected'; ?>>1B</option>
+                        <option value="1C" <?php if ($user_class == '1C') echo 'selected'; ?>>1C</option>
+                        <option value="1D" <?php if ($user_class == '1D') echo 'selected'; ?>>1D</option>
+                        <option value="1E" <?php if ($user_class == '1E') echo 'selected'; ?>>1E</option>
+                        <option value="1F" <?php if ($user_class == '1F') echo 'selected'; ?>>1F</option>
+                        <option value="1G" <?php if ($user_class == '1G') echo 'selected'; ?>>1G</option>
+
+                        <option value="" disabled>Seconde</option>
+                        <option value="2A" <?php if ($user_class == '2A') echo 'selected'; ?>>2A</option>
+                        <option value="2B" <?php if ($user_class == '2B') echo 'selected'; ?>>2B</option>
+                        <option value="2C" <?php if ($user_class == '2C') echo 'selected'; ?>>2C</option>
+                        <option value="2D" <?php if ($user_class == '2D') echo 'selected'; ?>>2D</option>
+                        <option value="2E" <?php if ($user_class == '2E') echo 'selected'; ?>>2E</option>
+                        <option value="2F" <?php if ($user_class == '2F') echo 'selected'; ?>>2F</option>
+                        <option value="2G" <?php if ($user_class == '2G') echo 'selected'; ?>>2G</option>
+
+                        <option value="" disabled>Terze</option>
+                        <option value="3A" <?php if ($user_class == '3A') echo 'selected'; ?>>3A</option>
+                        <option value="3B" <?php if ($user_class == '3B') echo 'selected'; ?>>3B</option>
+                        <option value="3C" <?php if ($user_class == '3C') echo 'selected'; ?>>3C</option>
+                        <option value="3D" <?php if ($user_class == '3D') echo 'selected'; ?>>3D</option>
+                        <option value="3E" <?php if ($user_class == '3E') echo 'selected'; ?>>3E</option>
+                        <option value="3F" <?php if ($user_class == '3F') echo 'selected'; ?>>3F</option>
+                        <option value="3G" <?php if ($user_class == '3G') echo 'selected'; ?>>3G</option>
+
+                        <option value="" disabled>Quarte</option>
+                        <option value="4A" <?php if ($user_class == '4A') echo 'selected'; ?>>4A</option>
+                        <option value="4B" <?php if ($user_class == '4B') echo 'selected'; ?>>4B</option>
+                        <option value="4C" <?php if ($user_class == '4C') echo 'selected'; ?>>4C</option>
+                        <option value="4D" <?php if ($user_class == '4D') echo 'selected'; ?>>4D</option>
+                        <option value="4E" <?php if ($user_class == '4E') echo 'selected'; ?>>4E</option>
+                        <option value="4F" <?php if ($user_class == '4F') echo 'selected'; ?>>4F</option>
+                        <option value="4G" <?php if ($user_class == '4G') echo 'selected'; ?>>4G</option>
+
+                        <option value="" disabled>Quinte</option>
+                        <option value="5A" <?php if ($user_class == '5A') echo 'selected'; ?>>5A</option>
+                        <option value="5B" <?php if ($user_class == '5B') echo 'selected'; ?>>5B</option>
+                        <option value="5C" <?php if ($user_class == '5C') echo 'selected'; ?>>5C</option>
+                        <option value="5D" <?php if ($user_class == '5D') echo 'selected'; ?>>5D</option>
+                        <option value="5E" <?php if ($user_class == '5E') echo 'selected'; ?>>5E</option>
+                        <option value="5F" <?php if ($user_class == '5F') echo 'selected'; ?>>5F</option>
+                        <option value="5G" <?php if ($user_class == '5G') echo 'selected'; ?>>5G</option>
+                    </select>
+
+                </div>
+                <div>
+                    <label for="mail">Email</label>
+                    <input type="email" id="mail" name="mail" value="<?php echo htmlspecialchars($user_mail); ?>" readonly>
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" value="<?php echo htmlspecialchars($user_password); ?>" readonly>
+                </div>
+                <div>
+                    <input type="hidden" name="button" value="delete">
+                    <button type="button" id="editButton" onclick="enableEditing()">Modifica</button>
+                    <button type="submit" name="button" id="updateButton" value="update" style="display:none;">Aggiorna</button>
+                    <a href="index.php"><button type="button" id="cancelButton" style="display:none;">Annulla</button></a>
+                    <button type="button" name="button" id="deleteButton" style="display:none;" onclick="confermaEliminazione()">Elimina account</button></a>
+                </div>
+            </form>
+        </div>
 </main>
 
 <footer>
---
+++ account/update.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /HighSchoolPrints/login/index.php?next=account");
+    exit();
+}
+
+$_user_id = $_SESSION['user_id'];
+$_button_id = $_POST['button'];
+//prendi i dati inseriti dall'utente
+$_name = $_POST['name'];
+$_surname = $_POST['surname'];
+$_mail = $_POST['mail'];
+$_password = $_POST['password'];
+$_class = $_POST['class'];
+
+$_dbuser = "root";
+$_dbpass = "root";
+$_dbhost = "localhost";
+$_dbname = "HSPrintsDB";
+$_conn = mysqli_connect($_dbhost, $_dbuser, $_dbpass, $_dbname);
+if (!$_conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    if($_button_id == "delete") {
+        $_query = "SET foreign_key_checks = 0;";
+        mysqli_query($_conn, $_query);
+        $_query = <<<EOF
+            DELETE FROM Students
+            WHERE id='$_user_id';
+        EOF;
+        $_result = mysqli_query($_conn, $_query);
+        $_query = "SET foreign_key_checks = 1;";
+        mysqli_query($_conn, $_query);
+        session_destroy();
+        header("Location: /HighSchoolPrints/");
+    }
+    else if($_button_id == "update") {
+        $_query = <<<EOF
+        UPDATE Students
+        SET FName='$_name', LName='$_surname', Mail='$_mail', Password='$_password', Class='$_class'
+        WHERE ID='$_user_id';
+        EOF;
+        $_result = mysqli_query($_conn, $_query);
+    }
+    header("Location: /HighSchoolPrints/account/");
+    mysqli_close($_conn);
+}
